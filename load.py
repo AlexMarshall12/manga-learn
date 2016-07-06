d63c2850ee5f999c6995416a5eeb5bb07a43554d
Alex Marshall <alex@conduce.com>
2016-07-06 16:37:24 -0700

    using compressed hdf5 files now

--- load.py
+++ load.py
@@ -1,6 +1,12 @@
 import h5py
 import scipy as sp
+import boto3
+import glob
+import numpy as np
+import random
 
+from sklearn.externals import joblib
+from skimage import color,io
 from keras import backend as K
 from keras.models import Sequential
 from keras.utils.io_utils import HDF5Matrix
@@ -8,6 +14,9 @@ from keras.layers import Dense, Dropout, Activation, Flatten, normalization, con
 from keras.layers.convolutional import ZeroPadding2D
 from keras.layers import Convolution2D, MaxPooling2D,Reshape
 from keras.optimizers import SGD
+from keras.optimizers import Adadelta
+
+KNN = joblib.load('KNN.pkl')
 
 def extract_hypercolumn(model, layer_indexes, instance):
     layers = [model.layers[li].output for li in layer_indexes]
@@ -23,42 +32,58 @@ def extract_hypercolumn(model, layer_indexes, instance):
 
     return np.asarray(hypercolumns)
 
-def generate_batch_from_hdf5():
-    f = h5py.File("raw_tensors.h5","r")
-    dset_X = f.get('X')
-    dset_y = f.get('y')
-     
-    print dset_X.shape,dset_y.shape
-    total_data_size=dset_X.shape[0]
-    for i in range(dset_X.shape[0]):
-        X = dset_X[i:i+1,:,:,:]
-        print "1 X instance from hdf5 file has a shape of: ",X.shape
-        X = np.tile(X,(1,3,1,1)    #triplicate L values to feed into VGG16
-        hc = extract_hypercolumn(model,[3,8,15,22,29],X)
-        yield dset_X[i:i+1,:,:,:],dset_y[i:i+1,:,:]
+def get_arrays(each_file):
+    lab = color.rgb2lab(io.imread(each_file))
+    X = lab[:,:,:1]
+    y = lab[:,:,1:]
+    X_rows,X_columns,X_channels=X.shape
+    y_rows,y_columns,y_channels=y.shape
+    X_channels_first = np.transpose(X,(2,0,1))
+    X_sample = np.expand_dims(X_channels_first,axis=0)
+    X_3d = np.tile(X_sample,(1,3,1,1))
+    hc = extract_hypercolumn(model,[3,8,15,22],X_3d)
+    hc_expand_dims = np.expand_dims(hc,axis=0)
+    y_reshaped = np.reshape(y,(y_rows*y_columns,y_channels))
+    classed_pixels_first = KNN.predict_proba(y_reshaped)
+    classed_classes_first = np.transpose(classed_pixels_first,(1,0))
+    classed_expand_dims = np.expand_dims(classed_classes_first,axis=0)
+    print "hypercolumn shape: ",hc_expand_dims.shape,"classified output color shape: ",classed_expand_dims.shape
+    return hc_expand_dims,classed_expand_dims
+
+def generate_batch():
+    files = glob.glob('../manga-resized/sliced/*.png')
+    random.shuffle(files)
+    counter = 0
+    while True:
+        for fl in files: 
+            counter+=1
+            print counter
+            if counter % 1000 == 0:
+                colorize.save_weights('colorize_weights.h5',overwrite=True)
+            yield get_arrays(fl)
 
 def Colorize(weights_path=None):
     model = Sequential()
     # input: 100x100 images with 3 channels -> (3, 100, 100) tensors.
     # this applies 32 convolution filters of size 3x3 each.
 
-    model.add(Convolution2D(512, 1, 1, border_mode='valid',input_shape=(963,224,224)))
+    model.add(Convolution2D(512, 1, 1, border_mode='valid',input_shape=(960,224,224)))
     model.add(Activation('relu'))
     model.add(normalization.BatchNormalization())
 
     model.add(Convolution2D(256, 1, 1, border_mode='valid'))
-    model.add(convolutional.ZeroPadding2D(padding=(1,1)))
+#    model.add(convolutional.ZeroPadding2D(padding=(1,1)))
     model.add(Activation('relu'))
     model.add(normalization.BatchNormalization())
 
     model.add(Convolution2D(112, 1, 1, border_mode='valid'))
-    model.add(convolutional.ZeroPadding2D(padding=(1,1)))
+ #   model.add(convolutional.ZeroPadding2D(padding=(1,1)))
     model.add(Activation('relu'))
     model.add(normalization.BatchNormalization())
    
     print "output shape: ",model.output_shape
     #softmax
-    model.add(Reshape((512,224*224)))
+    model.add(Reshape((112,224*224)))
 
     print "output_shape after reshaped: ",model.output_shape
     model.add(Activation('softmax'))
@@ -120,11 +145,17 @@ def VGG_16(weights_path='vgg16_weights.h5'):
 
 
 model = VGG_16()
-sgd = SGD(lr=0.1,decay=1e-6,momentum=0.9,nesterov=True)
-model.compile(optimizer=sgd,loss='categorical_crossentropy',metrics=["accuracy"])
+sgd = SGD(lr=0.01,decay=1e-6,momentum=0.9,nesterov=True)
+model.compile(optimizer=sgd,loss='categorical_crossentropy')
 
-#for fl in files: 
-#    out = model.predict(color.rgb2lab(fl))
-#    hc = extract_hypercolumn(model, layers_extract = [3,8])
+colorize = Colorize()
+color_sgd = SGD(lr=0.0001)
+color_adadelta = Adadelta()
+colorize.compile(optimizer=color_adadelta,loss='categorical_crossentropy',metrics=["accuracy"])
 
-model.fit_generator(generate_batch_from_hdf5(),samples_per_epoch=1000,nb_epoch=5)
+#from itertools import islice
+#print list(islice(generate_batch(), 3))
+#colorize.fit_generator(generate_batch(),samples_per_epoch=1,nb_epoch=5)
+#for a, b in generate_batch(): print(a, b)
+
+colorize.fit_generator(generate_batch(),samples_per_epoch=20,nb_epoch=1)
--- populate_h5.py
+++ populate_h5.py
@@ -1,36 +1,150 @@
 import h5py
+import tables
 from itertools import izip_longest
 import glob
+import boto3
 import numpy as np
+import scipy as sp
+from PIL import Image
+
 from sklearn.externals import joblib
 from skimage import color,io
-KNN = joblib.load('KNN.pkl')
+
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.convolutional import ZeroPadding2D
+from keras.optimizers import SGD
 
 
-files = glob.glob('../manga-resized/sliced_images/*.png')
-f = h5py.File('raw_tensors.h5','w')
-dset_X = f.create_dataset('X',(1,1,224,224),maxshape=(None,1,224,224),chunks=True)
-dset_y = f.create_dataset('y',(1,112,224*224),maxshape=(None,112,224*224),chunks=True)
+def VGG_16(weights_path='vgg16_weights.h5'):
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-n_images = 0
-for fl in files:
-    img = color.rgb2lab(io.imread(fl)[..., :3])
-    X = img[:,:,:1]
-    y = img[:,:,1:]
-    print "y shape: ",y.shape
-    print "X shape: ",X.shape
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1000, activation='softmax'))
+    
+    if weights_path:
+        model.load_weights(weights_path)
+
+    return model
+
+def get_arrays(each_file):
+    lab = color.rgb2lab(io.imread(each_file))
+    X = lab[:,:,:1]
+    y = lab[:,:,1:]
     X_rows,X_columns,X_channels=X.shape
     y_rows,y_columns,y_channels=y.shape
-    X_chunk = np.transpose(X,(2,0,1))
-    y_chunk = np.reshape(y,(y_rows*y_columns,y_channels))
-    classed = KNN.predict_proba(y_chunk)
-    classed = np.transpose(classed,(1,0))
-    dset_X.resize(n_images+1,axis=0)
-    dset_y.resize(n_images+1,axis=0)
-    print "X_chunk: ",X_chunk.shape,"dset_X: ",dset_X.shape
-    print "y_chunk: ",classed.shape,"dset_y: ",dset_y.shape
-    dset_X[n_images:n_images+1,:,:,:]=X_chunk
-    dset_y[n_images:n_images+1,:,:]=classed
-    n_images+= 1
-    print dset_X.shape
-    print dset_y.shape
+    X_channels_first = np.transpose(X,(2,0,1))
+    X_sample = np.expand_dims(X_channels_first,axis=0)
+    X_3d = np.tile(X_sample,(1,3,1,1))
+    hc = extract_hypercolumn(model,[3,8,15,22],X_3d)
+    hc_expand_dims = np.expand_dims(hc,axis=0)
+    y_reshaped = np.reshape(y,(y_rows*y_columns,y_channels))
+    classed_pixels_first = KNN.predict_proba(y_reshaped)
+    classed_classes_first = np.transpose(classed_pixels_first,(1,0))
+    classed_expand_dims = np.expand_dims(classed_classes_first,axis=0)
+    print "hypercolumn shape: ",hc_expand_dims.shape,"classified output color shape: ",classed_expand_dims.shape
+    return hc_expand_dims,classed_expand_dims
+
+def extract_hypercolumn(model, layer_indexes, instance):
+    layers = [model.layers[li].output for li in layer_indexes]
+    get_feature = K.function([model.layers[0].input],layers)
+    assert instance.shape == (1,3,224,224)
+    feature_maps = get_feature([instance])
+    hypercolumns = []
+    for convmap in feature_maps:
+        for fmap in convmap[0]:
+            upscaled = sp.misc.imresize(fmap, size=(224, 224),
+                                        mode="F", interp='bilinear')
+            hypercolumns.append(upscaled)
+
+    return np.asarray(hypercolumns)
+
+
+KNN = joblib.load('KNN.pkl')
+s3 = boto3.resource('s3')
+
+files = glob.glob('../manga-resized/sliced/*.png')
+model = VGG_16()
+sgd = SGD(lr=0.1,decay=1e-6,momentum=0.9,nesterov=True)
+model.compile(optimizer=sgd,loss='categorical_crossentropy')
+
+#h5file = open_file("tutorial1.h5", mode = "w", title = "Test file")
+
+import tables
+
+filters = tables.Filters(complevel=5, complib='zlib')
+
+with tables.openFile('raw.h5','w') as f:
+#    filters = tables.Filters(complib='blosc', complevel=5)
+    dset_X = f.create_earray(f.root, 'X', tables.Atom.from_dtype(np.dtype('Float64')), (0,960,224,224),filters=filters)
+    set_y = f.create_earray(f.root, 'y', tables.Atom.from_dtype(np.dtype('Float64')), (0,112,224*224),filters=filters)
+    for fl in files[0:12000]:
+        img = Image.open(fl)
+        if img.getbbox():
+            X_chunk,y_chunk=get_arrays(fl)
+            print X_chunk.dtype
+            dset_X.append(X_chunk)
+            dset_y.append(y_chunk)
+        else:
+            print "was black"
+            pass
+    
+
+#n_images = 0
+#
+#with h5py.File('image1.h5','w') as f: 
+#    dset_X = f.create_dataset('X',(1,960,224,224),maxshape=(None,960,224,224),chunks=True,compression='gzip', compression_opts=5)
+#    dset_y = f.create_dataset('y',(1,112,224*224),maxshape=(None,112,224*224),chunks=True,compression='gzip', compression_opts=5)
+#    n_images = 0
+#    for fl in files[200:236]:
+#        X_chunk,y_chunk = get_arrays(fl) 
+#        dset_X.resize(n_images+1,axis=0,)
+#        dset_y.resize(n_images+1,axis=0,)
+#        print dset_X.compression,dset_y.compression
+#        print dset_X.shape,dset_y.shape
+#        dset_X[n_images:n_images+1,:,:,:]=X_chunk
+#        dset_y[n_images:n_images+1,:,:]=y_chunk
+#        n_images+=1
+#    #s3.Bucket('my-bucket').put_object(Key='test.jpg', Body=f)
+    
